0e1ca0ac1e08963a4212b5c10316f880074a4de2
figorr <jdcuartero@yahoo.es>
2026-07-01 17:34:24 +0200

    Add pyproject.toml with semantic-release config

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 version="$1"
 
+# pyproject.toml
+sed -i "s/^version = \".*\"/version = \"$version\"/" pyproject.toml
+
 # manifest.json
 jq --arg ver "$version" '.version = $ver' custom_components/fanpy/manifest.json > tmp.json && mv tmp.json custom_components/fanpy/manifest.json
 
