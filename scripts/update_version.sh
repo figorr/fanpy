19b0238aa08a6463c68be8081f7a8304ffc8c0fb
figorr <jdcuartero@yahoo.es>
2026-07-02 18:22:33 +0200

    ci: remove npm publish, fix package-lock sync

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -12,10 +12,4 @@ jq --arg ver "$version" '.version = $ver' custom_components/fanpy/manifest.json 
 # version.py
 echo "__version__ = \"$version\"" > custom_components/fanpy/version.py
 
-# package.json
-jq --arg ver "$version" '.version = $ver' package.json > tmp.json && mv tmp.json package.json
-
-# package-lock.json
-jq --arg ver "$version" '.version = $ver | .packages[""].version = $ver' package-lock.json > tmp.json && mv tmp.json package-lock.json
-
 rm -f tmp.json
